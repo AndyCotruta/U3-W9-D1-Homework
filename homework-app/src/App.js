e3aa106213a7cceda3f6950ae227e9edaa8f8713
AndyCotruta <acotruta18@gmail.com>
2022-11-21 16:55:50 +0100

    trying to implement custom image size

--- homework-app/src/App.js
+++ homework-app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import FirstComponent from "./components/FirstComponent";
 import FirstClassComponent from "./components/FirstClassComponent";
+import InputFields from "./components/InputFields";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <InputFields />
         <FirstComponent buttonLabel="Click to shuffle" />
         <FirstClassComponent
           imageSource="https://picsum.photos/500/400"
---
+++ homework-app/src/components/InputFields.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const InputFields = () => {
+  const [width, setWidth] = useState("");
+  const [height, setHeight] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const imageSize = { width, height };
+    console.log(imageSize);
+  };
+  return (
+    <div className="pick-size">
+      <h2>Pick image size</h2>
+      <form onSubmit={handleSubmit} className="form-area">
+        <div className="pick">
+          <label>Width</label>
+          <input
+            type="number"
+            required
+            value={width}
+            onChange={(e) => setWidth(e.target.value)}
+          />
+        </div>
+        <div className="pick">
+          <label>Height</label>
+          <input
+            type="number"
+            required
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </div>
+        <button>Change size!</button>
+      </form>
+    </div>
+  );
+};
+
+export default InputFields;
